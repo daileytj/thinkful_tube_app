6ee4f32757f2130c970910d151dce8df28ad3ca6
Thomas Dailey <daileytj89@gmail.com>
2016-12-31 12:10:21 -0500

    wireframe update

---
+++ app.js
@@ -0,0 +1,17 @@
+// Application state *
+
+// declare params object
+
+// Application functionality *
+
+// on user ".search_button" click or "enter" keypress store ".search" input value
+
+// make api call using stored ".search" input value
+
+// Populate ".results_section" with results using
+// <div class="result">
+//        <p class="video_title">Video Title</p>
+//        <img src="img_source" class="video_tumbnail" alt="video thumbnail" />
+//    </div>
+
+$(".search_button").on("click", alert("button clicked!"));
