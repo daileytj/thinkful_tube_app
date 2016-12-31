9d0736ff2e3f4090257e5cb7df7d12c6f0045bba
Thomas Dailey <daileytj89@gmail.com>
2016-12-31 15:27:35 -0500

    thinkful tube api hack complete

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
 // Application state *
 
-// declare params object
+// Global variables
+var user_search_string = "";
+var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+var API_KEY = 'AIzaSyAj1RKJlNaMlg3rfFolFXPzl7bC9d4ehhE';
+// Get data from API
+function getDataFromApi(searchTerm, callback) {
+  var settings = {
+    url: YOUTUBE_BASE_URL,
+    data: {
+      part: 'snippet',
+      key: API_KEY,
+      maxResults: 18,
+      q: searchTerm,
+      r: 'json'
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callback
+  };
+  $.ajax(settings);
+  console.log($.ajax(settings));
+}
+
+function displayYouTubeSearchData(data) {
+  var renderedHTML = '';
+  if (data.items) {
+    data.items.forEach(function(item) {
+     renderedHTML += '<div class="result">';
+     renderedHTML += '<p class="video_title">' + item.snippet.title + '</p>';
+     renderedHTML += '<a href = "https://www.youtube.com/watch?v=' + item.id.videoId + '"target="_blank"><img src="' + item.snippet.thumbnails.medium.url + '" class="video_tumbnail" alt="video thumbnail" /></a>';
+     renderedHTML += '</div>';
+    });
+  }
+  else {
+    renderedHTML += '<div class="result">';
+    renderedHTML += '<p class="video_title">No Video Found</p>';
+    renderedHTML += '</div>';
+  }
+
+  $(".results_section").html(renderedHTML);
+}
+
 
 // Application functionality *
-
+$(document).ready(function(){
 // on user ".search_button" click or "enter" keypress store ".search" input value
+$(".search").keypress(function(event){
+  if(event.keyCode == 13){
+    user_search_string = $(".search").val();
+    console.log("user_search_string: " + user_search_string);
+    getDataFromApi(user_search_string, displayYouTubeSearchData);
+    $(".divider").removeClass("hidden");
+    $(".results_section").removeClass("hidden");
+  }
+});
 
-// make api call using stored ".search" input value
-
-// Populate ".results_section" with results using
-// <div class="result">
-//        <p class="video_title">Video Title</p>
-//        <img src="img_source" class="video_tumbnail" alt="video thumbnail" />
-//    </div>
-
-$(".search_button").on("click", alert("button clicked!"));
+});
